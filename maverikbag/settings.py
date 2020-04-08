57b3922fe4fddaade7a7a538b9393d594f256499
Shrinidhi N Hegde <shrinidhinhegde@gmail.com>
2020-04-08 11:55:55 +0530

    Update settings.py

--- maverikbag/settings.py
+++ maverikbag/settings.py
@@ -127,8 +127,8 @@ STATICFILES_DIRS = [
 ]
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "AKIAWPDS6B5LRU7DPJYT")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "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")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "//accesskey")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "//secretkey")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "maverickbag")
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_CUSTOM_DOMAIN = os.environ.get("AWS_S3_CUSTOM_DOMAIN", "maverickbag.s3.amazonaws.com")
